7ff50b840c822ee03742640af4aae94fbb6ed67b
RKostyukov <roma1997sport@mail.ru>
2024-02-18 00:20:01 +0300

    second commit

--- src/ts/consts.ts
+++ src/ts/consts.ts
@@ -1,13 +1,18 @@
 import { AGENT_STATUS, EXPRESSION_STATUS } from "./enums";
 
+const greenIcon = "https://thumbs.dreamstime.com/b/%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0%D1%8F-%D0%B3%D0%B0%D0%BB%D0%BE%D1%87%D0%BA%D0%B0-%D0%BF%D0%BE%D0%B4%D1%82%D0%B2%D0%B5%D1%80%D0%B6%D0%B4%D0%B0%D1%8E%D1%82-%D0%B8%D0%BB%D0%B8-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8F-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B8-%D0%BA%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%BF%D0%BE%D0%BC%D0%B5%D1%82%D0%BA%D0%B8-185588596.jpg";
+const redIcon = "https://cdn-icons-png.flaticon.com/512/6368/6368418.png";
+const yellowIcon = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeendsSC1KLb1ljM9hILUWPjJDcu_JPgdvsymh6DZj0YqBILgplXiIpWlELjhynu4R-9M&usqp=CAU";
+const blackIcon = "https://cdn-icons-png.flaticon.com/512/7351/7351882.png";
+
 export const ICONS = {
-  [AGENT_STATUS.RUNNING]: "https://cdn-icons-png.flaticon.com/512/7351/7351882.png",
-  [AGENT_STATUS.SLEEPING]: "https://thumbs.dreamstime.com/b/%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0%D1%8F-%D0%B3%D0%B0%D0%BB%D0%BE%D1%87%D0%BA%D0%B0-%D0%BF%D0%BE%D0%B4%D1%82%D0%B2%D0%B5%D1%80%D0%B6%D0%B4%D0%B0%D1%8E%D1%82-%D0%B8%D0%BB%D0%B8-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8F-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B8-%D0%BA%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%BF%D0%BE%D0%BC%D0%B5%D1%82%D0%BA%D0%B8-185588596.jpg",
-  [AGENT_STATUS.WAITING]: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeendsSC1KLb1ljM9hILUWPjJDcu_JPgdvsymh6DZj0YqBILgplXiIpWlELjhynu4R-9M&usqp=CAU",
-  [AGENT_STATUS.TERMINATED]: "https://cdn-icons-png.flaticon.com/512/6368/6368418.png",
-  [EXPRESSION_STATUS.READY_FOR_COMPUTATION]: "https://cdn-icons-png.flaticon.com/512/7351/7351882.png",
-  [EXPRESSION_STATUS.RESULT]: "https://thumbs.dreamstime.com/b/%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0%D1%8F-%D0%B3%D0%B0%D0%BB%D0%BE%D1%87%D0%BA%D0%B0-%D0%BF%D0%BE%D0%B4%D1%82%D0%B2%D0%B5%D1%80%D0%B6%D0%B4%D0%B0%D1%8E%D1%82-%D0%B8%D0%BB%D0%B8-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8F-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B8-%D0%BA%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%BF%D0%BE%D0%BC%D0%B5%D1%82%D0%BA%D0%B8-185588596.jpg",
-  [EXPRESSION_STATUS.COMPUTING]: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeendsSC1KLb1ljM9hILUWPjJDcu_JPgdvsymh6DZj0YqBILgplXiIpWlELjhynu4R-9M&usqp=CAU",
+  [AGENT_STATUS.RUNNING]: blackIcon,
+  [AGENT_STATUS.SLEEPING]: greenIcon,
+  [AGENT_STATUS.WAITING]: yellowIcon,
+  [AGENT_STATUS.TERMINATED]: redIcon,
+  [EXPRESSION_STATUS.READY_FOR_COMPUTATION]: blackIcon,
+  [EXPRESSION_STATUS.RESULT]: greenIcon,
+  [EXPRESSION_STATUS.COMPUTING]: yellowIcon,
 } as const;
 
 export const AGENT_DESCRIPTION = {
